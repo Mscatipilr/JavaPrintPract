95c6a4efa2adcca7d5da0357b71b02bc27e58941
Joann Ellis <joannpellis@gmail.com>
2024-07-22 16:53:50 -0400

    update exception

--- PrintingPractice/src/main/java/MyNameIs.java
+++ PrintingPractice/src/main/java/MyNameIs.java
@@ -1,5 +1,5 @@
 public class MyNameIs {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("My name is");
         System.out.println("Joann");
         System.out.println("Ellis");
--- PrintingPractice/src/main/java/Vacation.java
+++ PrintingPractice/src/main/java/Vacation.java
@@ -1,5 +1,5 @@
 public class Vacation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("I'd like to visit");
         System.out.println("Paris");
         System.out.println("France");
